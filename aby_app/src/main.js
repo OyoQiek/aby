3214d6480250780c697c47af01846cdd80e7f81a
xwx <xwx@163.com>
2019-08-08 20:28:16 +0800

    a

--- aby_app/src/main.js
+++ aby_app/src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import MintUI from 'mint-ui'
 
+axios.defaults.baseURL="http://127.0.0.1:3000/"
+axios.defaults.withCredentials=true
+Vue.prototype.axios=axios
+Vue.use(MintUI)
 Vue.config.productionTip = false
 
 new Vue({
