0d5c59d6011cc735a405518412faeddf6f527bd5
xbz <heihei@163.com>
2019-08-08 09:02:34 +0800

    install axios

--- aby_app/src/main.js
+++ aby_app/src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
+axios.defaults.baseURL="http://127.0.0.1:3000/"
+axios.defaults.withCredentials=true
+Vue.use(axios)
 Vue.config.productionTip = false
 
 new Vue({
