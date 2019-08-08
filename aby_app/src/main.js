abe61d4b790213b5194a84e46b5eaf1a4bdd1bd1
xbz <heihei@163.com>
2019-08-08 09:36:17 +0800

    icon font

--- aby_app/src/main.js
+++ aby_app/src/main.js
@@ -3,9 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
-
+import MintUI from 'mint-ui'
+//引入mint-ui组件库的样式
+import 'mint-ui/lib/style.css'
+//引入字体图标
+import './font/iconfont.css'
 axios.defaults.baseURL="http://127.0.0.1:3000/"
 axios.defaults.withCredentials=true
+Vue.use(MintUI)
 Vue.use(axios)
 Vue.config.productionTip = false
 
