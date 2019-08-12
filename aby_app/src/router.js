6bec16614b28e72f98b540fb1c576e14d214dc6e
xwx <xwx@163.com>
2019-08-12 19:57:24 +0800

    search

--- aby_app/src/router.js
+++ aby_app/src/router.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Story from './views/Story.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    
+    {path:'/Story',component:Story}
   ]
 })
--- aby_app/src/views/story.vue
+++ aby_app/src/views/story.vue
@@ -1,10 +1,9 @@
 <template>
     <div>
-        <div>
-            <div>
-                gidasdas 
-            </div>
+        <div id="search">
+            <input type="text" placeholder="搜索杭州的美食故事">
         </div>
+        
     </div>    
 </template>
 <script>
@@ -13,5 +12,10 @@ export default {
 }
 </script>
 <style scoped>
-
+    #search input{
+        box-shadow:0px,0px,10px,0px,#666 ;
+        margin: 10px 5%;
+        width: 90%;
+        padding: 10px 0;
+    }
 </style>
