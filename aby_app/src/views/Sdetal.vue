e1719a658284106d09c9c13c44fe9f8f8eda2c0f
xwx <xwx@163.com>
2019-08-14 13:16:01 +0800

    swiper

---
+++ aby_app/src/views/Sdetal.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <carousel></carousel>
+    </div>
+</template>
+<script>
+import Carousel from "../components/carousel.vue"
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    components:{
+        carousel
+    }
+}
+</script>
+<style scoped>
+
+</style>
