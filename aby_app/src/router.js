6e28b062144fa9d138f0e61c018e70120b8a8bdd
xwx <xwx@163.com>
2019-08-15 13:58:34 +0800

    t

--- aby_app/src/router.js
+++ aby_app/src/router.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HDetail from './views/detail/HDetail'
 import Story from './views/story.vue'
+import Sdetal from './views/Sdetal.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {path:"/HDetail",component:HDetail},
     {path:'/Story',component:Story},
+    {path:'/Sdetal',component:Sdetal},
   ]
 })
