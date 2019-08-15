7876d304f0ccba8e43ce9e5b9d27d34ba1f9d78b
xbz <heihei@163.com>
2019-08-15 14:03:06 +0800

    remark

--- aby_app/src/router.js
+++ aby_app/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import HDetail from './views/detail/HDetail'
 import Story from './views/story.vue'
 import Abstract from "./components/hdetail/abstract"
+import Remark from './components/hdetail/remark'
 Vue.use(Router)
 
 export default new Router({
@@ -10,5 +11,6 @@ export default new Router({
     {path:"/HDetail",component:HDetail},
     {path:'/Story',component:Story},
     {path:"/HDetail/abstract",component:Abstract},
+    {path:"/HDetail/remark",component:Remark},
   ]
 })
